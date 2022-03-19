67bc28330c444bea99bc30ff14122147ffb78415
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 01:05:07 +0530

    Update errors.py

--- horisan/ex_plugins/errors.py
+++ horisan/ex_plugins/errors.py
@@ -2,7 +2,7 @@ import sys
 import traceback
 from functools import wraps
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
-from EmikoRobot import EVENT_LOGS, pbot as app
+from horisan import EVENT_LOGS, pbot as app
 
 
 def split_limits(text):
