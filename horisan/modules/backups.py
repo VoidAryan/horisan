6ed1a46e24d9fdf9004e37f33047493783ac8220
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 01:28:32 +0530

    Update backups.py

--- horisan/modules/backups.py
+++ horisan/modules/backups.py
@@ -5,23 +5,23 @@ from telegram import ParseMode, Message
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler
 
-import EmikoRobot.modules.sql.notes_sql as sql
-from EmikoRobot import dispatcher, LOGGER, OWNER_ID, JOIN_LOGGER, SUPPORT_CHAT
-from EmikoRobot.__main__ import DATA_IMPORT
-from EmikoRobot.modules.helper_funcs.chat_status import user_admin
-from EmikoRobot.modules.helper_funcs.alternate import typing_action
+import horisan.modules.sql.notes_sql as sql
+from horisan import dispatcher, LOGGER, OWNER_ID, JOIN_LOGGER, SUPPORT_CHAT
+from horisan.__main__ import DATA_IMPORT
+from horisan.modules.helper_funcs.chat_status import user_admin
+from horisan.modules.helper_funcs.alternate import typing_action
 
-# from EmikoRobot.modules.rules import get_rules
-import EmikoRobot.modules.sql.rules_sql as rulessql
+# from horisan.modules.rules import get_rules
+import horisan.modules.sql.rules_sql as rulessql
 
-# from EmikoRobot.modules.sql import warns_sql as warnssql
-import EmikoRobot.modules.sql.blacklist_sql as blacklistsql
-from EmikoRobot.modules.sql import disable_sql as disabledsql
+# from horisan.modules.sql import warns_sql as warnssql
+import horisan.modules.sql.blacklist_sql as blacklistsql
+from horisan.modules.sql import disable_sql as disabledsql
 
-# from EmikoRobot.modules.sql import cust_filters_sql as filtersql
-# import EmikoRobot.modules.sql.welcome_sql as welcsql
-import EmikoRobot.modules.sql.locks_sql as locksql
-from EmikoRobot.modules.connection import connected
+# from horisan.modules.sql import cust_filters_sql as filtersql
+# import horisan.modules.sql.welcome_sql as welcsql
+import horisan.modules.sql.locks_sql as locksql
+from horisan.modules.connection import connected
 
 
 @user_admin
