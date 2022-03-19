ad33c2f54b1be40e3fd5ba0f296959a4876c2103
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 00:27:27 +0530

    Update config.py

--- horisan/config.py
+++ horisan/config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/EmikoRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/horisan/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "excrybaby"
-    SUPPORT_CHAT = "emikosupport"  # Your own group for support, do not add the @
+    OWNER_ID = 2119580786  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "voidxtoxic"
+    SUPPORT_CHAT = "HoriXSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001432609692
+        -1001633814912
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001150905176
+        -1001633814912
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
