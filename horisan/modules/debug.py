fe18545c420a20de741e4f33027d2fdc4949ae55
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 01:54:28 +0530

    Update debug.py

--- horisan/modules/debug.py
+++ horisan/modules/debug.py
@@ -5,8 +5,8 @@ from telethon import events
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler
 
-from EmikoRobot import telethn, dispatcher
-from EmikoRobot.modules.helper_funcs.chat_status import dev_plus
+from horisan import telethn, dispatcher
+from horisan.modules.helper_funcs.chat_status import dev_plus
 
 DEBUG_MODE = False
 
