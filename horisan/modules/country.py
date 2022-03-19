dacb7f2535f7b9d68426f4b8c0192e604b849559
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 01:49:25 +0530

    Update country.py

--- horisan/modules/country.py
+++ horisan/modules/country.py
@@ -5,8 +5,8 @@ from telethon.utils import get_input_location
 import flag
 import html, os
 from countryinfo import CountryInfo
-from EmikoRobot import telethn as borg
-from EmikoRobot.events import register
+from horisan import telethn as borg
+from horisan.events import register
 
 
 @register(pattern="^/country (.*)")
