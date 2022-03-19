4c88b85db6053e2e38c774a9c6ea3b998fcd5633
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 01:18:13 +0530

    Update alive.py

--- horisan/modules/alive.py
+++ horisan/modules/alive.py
@@ -5,8 +5,8 @@ from telethon import events, Button
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from EmikoRobot.events import register
-from EmikoRobot import telethn as tbot
+from horisan.events import register
+from horisan import telethn as tbot
 
 
 PHOTO = "https://telegra.ph/file/c458924bf40c213062dd2.mp4"
@@ -17,11 +17,11 @@ async def awake(event):
     TEXT = f"**Hey There Weebs!![{event.sender.first_name}](tg://user?id={event.sender.id})** \n\n"
     TEXT += f"**I Am Hori San!**\n\n"
     TEXT += f"**Meet My izumi kun hehe - [VOID](https://t.me/voidxtoxic)**** \n\n"
-    TEXT += "**◈ I will love to be in your GC! hehe! ◈"
+    TEXT += "**◈ I will love to be in your groupchat ◈"
     BUTTON = [
         [
-            Button.url("【Support】", "https://t.me/bot_projectx"),
-            Button.url("【Network】", "https://t.me/void_network"),
+            Button.url("【Support】", "https://t.me/HoriXSupport"),
+            Button.url("【Updates】", "https://t.me/kyouko_updates"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
