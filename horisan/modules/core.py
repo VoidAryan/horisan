458cf7e6b0122e88a3746578ff2741575f85bb7b
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 01:48:06 +0530

    Update core.py

--- horisan/modules/core.py
+++ horisan/modules/core.py
@@ -1,16 +1,16 @@
-from EmikoRobot import telethn as tbot
-from EmikoRobot.events import register
+from horisan import telethn as tbot
+from horisan.events import register
 import os
 import asyncio
 import os
 import time
 from datetime import datetime
-from EmikoRobot import OWNER_ID, DEV_USERS
-from EmikoRobot import TEMP_DOWNLOAD_DIRECTORY as path
-from EmikoRobot import TEMP_DOWNLOAD_DIRECTORY
+from horisan import OWNER_ID, DEV_USERS
+from horisan import TEMP_DOWNLOAD_DIRECTORY as path
+from horisan import TEMP_DOWNLOAD_DIRECTORY
 from datetime import datetime
 
-water = "./EmikoRobot/resources/emiko.png"
+water = "./horisan/resources/hori.png"
 client = tbot
 
 
@@ -23,7 +23,7 @@ async def Prof(event):
     thumb = water
     message_id = event.message.id
     input_str = event.pattern_match.group(1)
-    the_plugin_file = "./EmikoRobot/modules/{}.py".format(input_str)
+    the_plugin_file = "./horisan/modules/{}.py".format(input_str)
     if os.path.exists(the_plugin_file):
         message_id = event.message.id
         await event.client.send_file(
