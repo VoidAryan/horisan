d009f934825c363ed46c59709eca5d9fe458563d
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 00:53:11 +0530

    Update pluginhelper.py

--- horisan/utils/pluginhelper.py
+++ horisan/utils/pluginhelper.py
@@ -13,8 +13,8 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from EmikoRobot import OWNER_ID, SUPPORT_CHAT
-from EmikoRobot import pbot
+from horisan import OWNER_ID, SUPPORT_CHAT
+from horisan import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
