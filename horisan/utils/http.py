0a00622c1153effdbf96a9f226b0820924adf087
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 00:36:24 +0530

    Update http.py

--- horisan/utils/http.py
+++ horisan/utils/http.py
@@ -19,7 +19,7 @@ SOFTWARE.
 """
 from asyncio import gather
 
-from EmikoRobot import aiohttpsession as session
+from horisan import aiohttpsession as session
 
 
 async def get(url: str, *args, **kwargs):
