7c83c9583ba2c784541065f036755e6c95547199
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 01:15:13 +0530

    Update admin.py

--- horisan/modules/admin.py
+++ horisan/modules/admin.py
@@ -5,9 +5,9 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from EmikoRobot import DRAGONS, dispatcher
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
-from EmikoRobot.modules.helper_funcs.chat_status import (
+from horisan import DRAGONS, dispatcher
+from horisan.modules.disable import DisableAbleCommandHandler
+from horisan.modules.helper_funcs.chat_status import (
     bot_admin,
     can_pin,
     can_promote,
@@ -16,16 +16,16 @@ from EmikoRobot.modules.helper_funcs.chat_status import (
     ADMIN_CACHE,
 )
 
-from EmikoRobot.modules.helper_funcs.admin_rights import (
+from horisan.modules.helper_funcs.admin_rights import (
     user_can_changeinfo,
     user_can_promote,
 )
-from EmikoRobot.modules.helper_funcs.extraction import (
+from horisan.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from EmikoRobot.modules.log_channel import loggable
-from EmikoRobot.modules.helper_funcs.alternate import send_message
+from horisan.modules.log_channel import loggable
+from horisan.modules.helper_funcs.alternate import send_message
 
 
 @bot_admin
