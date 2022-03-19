779bef2a4cbc614409dfbd37e020ca5e4f12a3e7
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 01:20:03 +0530

    Update anime.py

--- horisan/modules/anime.py
+++ horisan/modules/anime.py
@@ -5,8 +5,8 @@ import textwrap
 import bs4
 import jikanpy
 import requests
-from EmikoRobot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
+from horisan import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
+from horisan.modules.disable import DisableAbleCommandHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler
 
