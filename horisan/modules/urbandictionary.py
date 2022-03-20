dc2b1ce25426b3238498de1b45d0f7cf48625a0d
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:03:47 +0530

    Update urbandictionary.py

--- horisan/modules/urbandictionary.py
+++ horisan/modules/urbandictionary.py
@@ -1,6 +1,6 @@
 import requests
-from EmikoRobot import dispatcher
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
+from horisan import dispatcher
+from horisan.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
