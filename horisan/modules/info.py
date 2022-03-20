e085bbe171521dfae544877fb54e0193f735903e
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 07:20:17 +0530

    Update info.py

--- horisan/modules/info.py
+++ horisan/modules/info.py
@@ -3,9 +3,9 @@ import os
 from pyrogram import filters
 from pyrogram.types import Message
 
-from EmikoRobot import DEV_USERS
-from EmikoRobot import pbot as app
-from EmikoRobot.services.sections import section
+from horisan import DEV_USERS
+from horisan import pbot as app
+from horisan.services.sections import section
 
 
 async def get_user_info(user, already=False):
