d52e620d6d9b66553634a55be509373411052817
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 08:51:15 +0530

    Update stickers.py

--- horisan/modules/stickers.py
+++ horisan/modules/stickers.py
@@ -13,8 +13,8 @@ from telegram import TelegramError, Update
 from telegram.ext import CallbackContext
 from telegram.utils.helpers import mention_html
 
-from EmikoRobot import dispatcher
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
+from horisan import dispatcher
+from horisan.modules.disable import DisableAbleCommandHandler
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
