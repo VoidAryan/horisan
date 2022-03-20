30b26fa6b89aa580e376f5a04f45b1fe63981dc7
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:54:39 +0530

    Update night_mode_sql.py

--- horisan/modules/sql/night_mode_sql.py
+++ horisan/modules/sql/night_mode_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, String, UnicodeText
-from EmikoRobot.modules.sql import BASE, SESSION
+from horisan.modules.sql import BASE, SESSION
 
 
 class Nightmode(BASE):
