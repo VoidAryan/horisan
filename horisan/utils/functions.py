54c0ffdd04a8d53bdcd05eee0ede6b040671bc53
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 10:10:51 +0530

    Update functions.py

--- horisan/utils/functions.py
+++ horisan/utils/functions.py
@@ -32,9 +32,9 @@ import speedtest
 from PIL import Image, ImageDraw, ImageFilter, ImageFont
 from pyrogram.types import Message
 
-from EmikoRobot import aiohttpsession as aiosession
-from EmikoRobot.ex_plugins.dbfunctions import start_restart_stage
-from EmikoRobot.utils.http import get, post
+from horisan import aiohttpsession as aiosession
+from horisan.ex_plugins.dbfunctions import start_restart_stage
+from horisan.utils.http import get, post
 
 
 async def restart(m: Message):
