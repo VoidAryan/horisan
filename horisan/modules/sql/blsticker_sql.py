82ab7397ab503ed19aeb2f8f2c621eb33f442a2a
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:44:18 +0530

    Update blsticker_sql.py

--- horisan/modules/sql/blsticker_sql.py
+++ horisan/modules/sql/blsticker_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, distinct, func, Integer
 
-from EmikoRobot.modules.sql import BASE, SESSION
+from horisan.modules.sql import BASE, SESSION
 
 
 class StickersFilters(BASE):
