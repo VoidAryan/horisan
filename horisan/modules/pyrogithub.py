b44958245dcf3c20842824f36c9c9ff2daa0c570
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 07:52:36 +0530

    Update pyrogithub.py

--- horisan/modules/pyrogithub.py
+++ horisan/modules/pyrogithub.py
@@ -1,7 +1,7 @@
 import aiohttp
 from pyrogram import filters
-from EmikoRobot import pbot, BOT_USERNAME
-from EmikoRobot.utils.errors import capture_err
+from horisan import pbot, BOT_USERNAME
+from horisan.utils.errors import capture_err
 
 
 __mod_name__ = "Github"
