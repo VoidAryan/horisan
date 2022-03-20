3b773aa2c716fa202b804d5fcb821f65c6e98583
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:42:49 +0530

    Update approve_sql.py

--- horisan/modules/sql/approve_sql.py
+++ horisan/modules/sql/approve_sql.py
@@ -3,7 +3,7 @@ import threading
 from sqlalchemy import Column, String
 from sqlalchemy.sql.sqltypes import BigInteger
 
-from EmikoRobot.modules.sql import BASE, SESSION
+from horisan.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
