ff368f520e22ab82aa87cf01ac8e589075da817e
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:36:21 +0530

    Update extraction.py

--- horisan/modules/helper_funcs/extraction.py
+++ horisan/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from EmikoRobot import LOGGER
-from EmikoRobot.modules.users import get_user_id
+from horisan import LOGGER
+from horisan.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
