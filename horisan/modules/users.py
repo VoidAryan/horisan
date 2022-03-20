ddcf5620502efc93f16d62cce60a4f4636cfb80a
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:06:46 +0530

    Update users.py

--- horisan/modules/users.py
+++ horisan/modules/users.py
@@ -10,10 +10,10 @@ from telegram.ext import (
     MessageHandler,
 )
 
-import EmikoRobot.modules.sql.users_sql as sql
-from EmikoRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from EmikoRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from EmikoRobot.modules.sql.users_sql import get_all_users
+import horisan.modules.sql.users_sql as sql
+from horisan import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from horisan.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from horisan.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
