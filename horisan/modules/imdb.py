fc72335b8fcd2be892819a0cbe6aa88f932406d2
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 07:16:43 +0530

    Update imdb.py

--- horisan/modules/imdb.py
+++ horisan/modules/imdb.py
@@ -1,11 +1,11 @@
-from EmikoRobot import telethn as tbot
+from horisan import telethn as tbot
 import os
 import re
 import bs4
 import requests
 from telethon import types
 from telethon.tl import functions
-from EmikoRobot.events import register
+from horisan.events import register
 
 langi = "en"
 
