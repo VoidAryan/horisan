0bb47781faee438d2d9a836f2f48ec60f1c936ee
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:40:36 +0530

    Update chatstatus.py

--- horisan/modules/helper_funcs/telethn/chatstatus.py
+++ horisan/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
-from EmikoRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from EmikoRobot import DRAGONS
+from horisan.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from horisan import DRAGONS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
