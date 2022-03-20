0e2ec10a99f6e94b5cf479ae38ff6dd5015a7ce2
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:55:21 +0530

    Update notes_sql.py

--- horisan/modules/sql/notes_sql.py
+++ horisan/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from EmikoRobot.modules.helper_funcs.msg_types import Types
-from EmikoRobot.modules.sql import BASE, SESSION
+from horisan.modules.helper_funcs.msg_types import Types
+from horisan.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, String, UnicodeText, distinct, func, Integer
 
 
