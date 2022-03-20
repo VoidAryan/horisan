2a2f088c8059b8fe0cf2bb9704fdd47d1631d0d2
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 07:31:46 +0530

    Update math.py

--- horisan/modules/math.py
+++ horisan/modules/math.py
@@ -1,8 +1,8 @@
 import math
 
 import pynewtonmath as newton
-from EmikoRobot import dispatcher
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
+from horisan import dispatcher
+from horisan.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
