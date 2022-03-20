e6b5970ba134962258f08e67a6ea36e2ef32270b
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 08:00:22 +0530

    Update reporting.py

--- horisan/modules/reporting.py
+++ horisan/modules/reporting.py
@@ -1,9 +1,9 @@
 import html
 
-from EmikoRobot import LOGGER, DRAGONS, TIGERS, WOLVES, dispatcher
-from EmikoRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from EmikoRobot.modules.log_channel import loggable
-from EmikoRobot.modules.sql import reporting_sql as sql
+from horisan import LOGGER, DRAGONS, TIGERS, WOLVES, dispatcher
+from horisan.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from horisan.modules.log_channel import loggable
+from horisan.modules.sql import reporting_sql as sql
 from telegram import Chat, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (
