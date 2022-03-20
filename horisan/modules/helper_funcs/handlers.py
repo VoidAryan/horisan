925c54fb4846a4afcb4ead3fe13ac9025a08e12a
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:37:54 +0530

    Update handlers.py

--- horisan/modules/helper_funcs/handlers.py
+++ horisan/modules/helper_funcs/handlers.py
@@ -1,6 +1,6 @@
-import EmikoRobot.modules.sql.blacklistusers_sql as sql
-from EmikoRobot import ALLOW_EXCL
-from EmikoRobot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
+import horisan.modules.sql.blacklistusers_sql as sql
+from horisan import ALLOW_EXCL
+from horisan import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
