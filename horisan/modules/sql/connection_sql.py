0884334d9f4e31104a02dc109b9492b6d872be33
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:46:02 +0530

    Update connection_sql.py

--- horisan/modules/sql/connection_sql.py
+++ horisan/modules/sql/connection_sql.py
@@ -5,7 +5,7 @@ from typing import Union
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 from sqlalchemy.sql.sqltypes import BigInteger
 
-from EmikoRobot.modules.sql import SESSION, BASE
+from horisan.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
