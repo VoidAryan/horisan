297ab7a879b00a66467588254dd1637f86b6ae9d
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 07:22:33 +0530

    Update json.py

--- horisan/modules/json.py
+++ horisan/modules/json.py
@@ -1,6 +1,6 @@
 import io
-from EmikoRobot.events import register
-from EmikoRobot import telethn as tbot
+from horisan.events import register
+from horisan import telethn as tbot
 from telethon import types
 from telethon import events
 from telethon.tl import functions
