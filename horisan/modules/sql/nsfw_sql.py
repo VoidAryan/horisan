dd7689e1115ee204976e59868affb05560de9338
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 10:01:57 +0530

    Update nsfw_sql.py

--- horisan/modules/sql/nsfw_sql.py
+++ horisan/modules/sql/nsfw_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from EmikoRobot.modules.sql import BASE, SESSION
+from horisan.modules.sql import BASE, SESSION
 
 #   |----------------------------------|
 #   |  Test Module by @EverythingSuckz |
