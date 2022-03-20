7fdeec0571567619d0916e0ecd31c4723e3c917e
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 07:57:10 +0530

    Update rembg.py

--- horisan/modules/rembg.py
+++ horisan/modules/rembg.py
@@ -25,9 +25,9 @@ import requests
 from telethon import types
 from telethon.tl import functions
 
-from EmikoRobot import REM_BG_API_KEY
-from EmikoRobot.events import register
-from EmikoRobot import telethn as tbot
+from horisan import REM_BG_API_KEY
+from horisan.events import register
+from horisan import telethn as tbot
 
 REM_BG_API_KEY = "REM_BG_API_KEY"
 TEMP_DOWNLOAD_DIRECTORY = "./"
