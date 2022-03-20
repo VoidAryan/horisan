77df7f2cd9fa026cd84dd543d84c132c760d6426
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 08:52:56 +0530

    Update styletext.py

--- horisan/modules/styletext.py
+++ horisan/modules/styletext.py
@@ -1,6 +1,6 @@
-from EmikoRobot import dispatcher
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
-from EmikoRobot.modules.helper_funcs.alternate import typing_action
+from horisan import dispatcher
+from horisan.modules.disable import DisableAbleCommandHandler
+from horisan.modules.helper_funcs.alternate import typing_action
 from telegram import ParseMode
 from telegram.ext import run_async
 
