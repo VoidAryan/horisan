64b4291a859ee68c2ee4fd7ce3dca04d9587eb0c
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:32:03 +0530

    Update texttospeech.py

--- horisan/modules/texttospeech.py
+++ horisan/modules/texttospeech.py
@@ -7,10 +7,10 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from EmikoRobot import *
+from horisan import *
 
-from EmikoRobot import telethn as tbot
-from EmikoRobot.events import register
+from horisan import telethn as tbot
+from horisan.events import register
 
 
 @register(pattern="^/tts (.*)")
