7584cc73441ea5991a423c2aab97e1c47622bf85
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:42:20 +0530

    Update antiflood_sql.py

--- horisan/modules/sql/antiflood_sql.py
+++ horisan/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, BigInteger, UnicodeText
 
-from EmikoRobot.modules.sql import SESSION, BASE
+from horisan.modules.sql import SESSION, BASE
 
 DEF_COUNT = 1
 DEF_LIMIT = 0
