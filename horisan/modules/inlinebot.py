f5f93f35d58947cdbca33fc03c0b4beaaa4b5f8c
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 07:21:28 +0530

    Update inlinebot.py

--- horisan/modules/inlinebot.py
+++ horisan/modules/inlinebot.py
@@ -1,8 +1,8 @@
 import traceback
 
-from EmikoRobot import pbot as app
-from EmikoRobot.utils.pluginhelper import fetch
-from EmikoRobot.utils.inlinefuncs import *
+from horisan import pbot as app
+from horisan.utils.pluginhelper import fetch
+from horisan.utils.inlinefuncs import *
 
 __MODULE__ = "Inline"
 __HELP__ = """See inline for help related to inline"""
