a88d8d5dd93982d13bafc3e1f9e07d38b3bb7a32
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:25:02 +0530

    Update zombies.py

--- horisan/modules/zombies.py
+++ horisan/modules/zombies.py
@@ -6,7 +6,7 @@ from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
 
-from EmikoRobot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
+from horisan import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
 
 # =================== CONSTANT ===================
 
