ea749a75a152fa366596f4f734de703fc2002fb5
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:48:35 +0530

    Update forceSubscribe_sql.py

--- horisan/modules/sql/forceSubscribe_sql.py
+++ horisan/modules/sql/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from EmikoRobot.modules.sql import BASE, SESSION
+from horisan.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
