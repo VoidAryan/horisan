f23d1ffc3e0b8c5192cf0d8247d413471faacfb1
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:23:31 +0530

    Update wiki.py

--- horisan/modules/wiki.py
+++ horisan/modules/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
-from EmikoRobot import dispatcher
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
+from horisan import dispatcher
+from horisan.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
