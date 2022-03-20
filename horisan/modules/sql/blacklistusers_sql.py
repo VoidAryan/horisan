0cc224625b0e4120d639d56ec4c020e683060b70
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-20 09:43:55 +0530

    Update blacklistusers_sql.py

--- horisan/modules/sql/blacklistusers_sql.py
+++ horisan/modules/sql/blacklistusers_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText
 
-from EmikoRobot.modules.sql import BASE, SESSION
+from horisan.modules.sql import BASE, SESSION
 
 
 class BlacklistUsers(BASE):
