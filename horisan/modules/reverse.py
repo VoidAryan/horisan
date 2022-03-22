e9d2d7ba29154934ddc8223f763992397469302d
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-22 21:06:38 +0530

    Update reverse.py

--- horisan/modules/reverse.py
+++ horisan/modules/reverse.py
@@ -183,7 +183,7 @@ Do you know that you can search an image with a link too? /reverse picturelink <
 __mod_name__ = "Reverse"
 
 REVERSE_HANDLER = DisableAbleCommandHandler(
-    ["pp", "grs" , "p","reverse"], reverse, pass_args=True, admin_ok=True
+    ["pp", "grs" , "p","r","reverse"], reverse, pass_args=True, admin_ok=True
 )
 
 dispatcher.add_handler(REVERSE_HANDLER)
