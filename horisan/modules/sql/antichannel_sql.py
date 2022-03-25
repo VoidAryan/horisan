54ca8f798b75672f6b3f4be55199e22dec64f396
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-25 08:06:02 +0530

    Update antichannel_sql.py

--- horisan/modules/sql/antichannel_sql.py
+++ horisan/modules/sql/antichannel_sql.py
@@ -4,7 +4,7 @@ from sqlalchemy import Boolean
 from sqlalchemy.sql.sqltypes import String
 from sqlalchemy import Column
 
-from MetaButler.modules.sql import BASE, SESSION
+from horisan.modules.sql import BASE, SESSION
 
 
 class AntiChannelSettings(BASE):
