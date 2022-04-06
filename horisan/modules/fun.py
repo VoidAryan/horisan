2b52099ce5dbea798657de8c25ce799334aef97b
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-06 16:44:41 +0530

    Update fun.py

--- horisan/modules/fun.py
+++ horisan/modules/fun.py
@@ -378,7 +378,7 @@ dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(EIGHTBALL_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
 
-__mod_name__ = "【Fun】"
+__mod_name__ = "【ꜰᴜɴ】"
 __command_list__ = [
     "runs",
     "slap",
