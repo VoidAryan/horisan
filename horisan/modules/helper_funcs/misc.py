97e67f094559bd1e2973d91f51bb5a201aa07d36
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-06 14:01:00 +0530

    Update misc.py

--- horisan/modules/helper_funcs/misc.py
+++ horisan/modules/helper_funcs/misc.py
@@ -50,14 +50,14 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
              in module_dict.values()])
 
     pairs = [
-    modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 3 - 1) // 2)
+    modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 2 - 1) // 2)
     ]
 
     round_num = len(modules) / 2
     calc = len(modules) - round(round_num)
     if calc == 1:
         pairs.append((modules[-1], ))
-    elif calc == 3:
+    elif calc == 2:
         pairs.append((modules[-1], ))
 
     max_num_pages = ceil(len(pairs) / 10)
