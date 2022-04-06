5f261cd0caaa7f40a9a0c3de9b745af3c094f8b2
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-06 16:17:08 +0530

    Update cleaner.py

--- horisan/modules/cleaner.py
+++ horisan/modules/cleaner.py
@@ -264,7 +264,7 @@ dispatcher.add_handler(REMOVE_CLEAN_BLUE_TEXT_GLOBAL_HANDLER)
 dispatcher.add_handler(LIST_CLEAN_BLUE_TEXT_HANDLER)
 dispatcher.add_handler(CLEAN_BLUE_TEXT_HANDLER, BLUE_TEXT_CLEAN_GROUP)
 
-__mod_name__ = "【Cleaner】"
+__mod_name__ = "【ᴄʟᴇᴀɴᴇʀ】"
 __handlers__ = [
     SET_CLEAN_BLUE_TEXT_HANDLER,
     ADD_CLEAN_BLUE_TEXT_HANDLER,
