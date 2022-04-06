7bb852f3b4d13a0681fda14c9a8e166c8fb1484e
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-06 11:43:56 +0530

    Update misc.py

--- horisan/modules/helper_funcs/misc.py
+++ horisan/modules/helper_funcs/misc.py
@@ -66,12 +66,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 9:
         pairs = pairs[modulo_page * 9:9 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("⌫", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("back",  url="https://t.me/eruXbakabot?start="),
-             EqInlineKeyboardButton("⌦", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("<", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("【 BACK 】",  url="https://t.me/kyoukohori_robot?start="),
+             EqInlineKeyboardButton(">", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="asuna_back")]]
+        pairs += [[EqInlineKeyboardButton("【 BACK 】", callback_data="asuna_back")]]
 
     return pairs
 
