add23d6e52e8c2f77b7326fa18b3125f7e7a60cd
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-06 16:40:41 +0530

    Update locks.py

--- horisan/modules/locks.py
+++ horisan/modules/locks.py
@@ -559,7 +559,7 @@ Locking bots will stop non-admins from adding bots to the chat.
 ❂ Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
 """
 
-__mod_name__ = "【Locks】"
+__mod_name__ = "【ʟᴏᴄᴋꜱ】"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes, run_async=True)
 LOCK_HANDLER = CommandHandler(
