26c6a4cacda36eb9c618bd92bc832af89dcce077
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-06 15:54:41 +0530

    Update anime.py

--- horisan/modules/anime.py
+++ horisan/modules/anime.py
@@ -606,7 +606,7 @@ dispatcher.add_handler(KAIZOKU_SEARCH_HANDLER)
 dispatcher.add_handler(KAYO_SEARCH_HANDLER)
 dispatcher.add_handler(UPCOMING_HANDLER)
 
-__mod_name__ = "【Anime/Manga】"
+__mod_name__ = "【ᴀɴɪᴍᴇ】"
 __command_list__ = [
     "anime",
     "manga",
