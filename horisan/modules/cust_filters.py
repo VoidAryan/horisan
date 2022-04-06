ee14ae097e1143a4514841d8ff7c9904df943b38
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-06 17:04:04 +0530

    Update cust_filters.py

--- horisan/modules/cust_filters.py
+++ horisan/modules/cust_filters.py
@@ -631,7 +631,7 @@ doin?
 Check /markdownhelp to know more!
 """
 
-__mod_name__ = "【Filters】"
+__mod_name__ = "【ꜰɪʟᴛᴇʀ】"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
