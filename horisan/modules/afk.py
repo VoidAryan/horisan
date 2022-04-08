a48d47d0ce80d194639855ffdd0d82a614129a81
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-09 00:29:26 +0530

    Update afk.py

--- horisan/modules/afk.py
+++ horisan/modules/afk.py
@@ -8,14 +8,14 @@ from telegram import MessageEntity, ParseMode
 from telegram.error import BadRequest
 from telegram.ext import Filters, MessageHandler, run_async
 
-from Senku import dispatcher
-from Senku.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
-from Senku.modules.redis.afk_redis import start_afk, end_afk, is_user_afk, afk_reason
-from Senku import REDIS
-from Senku.modules.users import get_user_id
+from horisan import dispatcher
+from horisan.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
+from horisan.modules.redis.afk_redis import start_afk, end_afk, is_user_afk, afk_reason
+from horisan import REDIS
+from horisan.modules.users import get_user_id
 
-from Senku.modules.helper_funcs.alternate import send_message
-from Senku.modules.helper_funcs.readable_time import get_readable_time
+from horisan.modules.helper_funcs.alternate import send_message
+from horisan.modules.helper_funcs.readable_time import get_readable_time
 
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
