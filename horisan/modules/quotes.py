6ce1a8183d1177b803bd887ad503ea86cd5bf501
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-10 20:54:42 +0530

    Update quotes.py

--- horisan/modules/quotes.py
+++ horisan/modules/quotes.py
@@ -4,9 +4,9 @@ from traceback import format_exc
 from pyrogram import filters
 from pyrogram.types import Message
 
+from horisan import pbot as app
 from horisan import arq
 from horisan.utils.errors import capture_err
-from horisan import pbot as app
 
 
 async def quotify(messages: list):
@@ -24,7 +24,7 @@ def getArg(message: Message) -> str:
     return arg
 
 
-def isArgInt(message: Message) -> bool:
+def isArgInt(message: Message) -> list:
     count = getArg(message)
     try:
         count = int(count)
@@ -33,14 +33,16 @@ def isArgInt(message: Message) -> bool:
         return [False, 0]
 
 
-@app.on_message(filters.command("q"))
+@app.on_message(filters.command("q") & ~filters.private)
 @capture_err
 async def quotly_func(client, message: Message):
     if not message.reply_to_message:
         return await message.reply_text("Reply to a message to quote it.")
     if not message.reply_to_message.text:
-        return await message.reply_text("Replied message has no text, can't quote it.")
-    m = await message.reply_text("Quoting Messages Please wait....")
+        return await message.reply_text(
+            "Replied message has no text, can't quote it."
+        )
+    m = await message.reply_text("Quoting Messages")
     if len(message.command) < 2:
         messages = [message.reply_to_message]
 
@@ -49,18 +51,24 @@ async def quotly_func(client, message: Message):
         if arg[0]:
             if arg[1] < 2 or arg[1] > 10:
                 return await m.edit("Argument must be between 2-10.")
+
             count = arg[1]
-            messages = await client.get_messages(
-                message.chat.id,
-                [
-                    i
-                    for i in range(
+
+            # Fetching 5 extra messages so tha twe can ignore media
+            # messages and still end up with correct offset
+            messages = [
+                i
+                for i in await client.get_messages(
+                    message.chat.id,
+                    range(
                         message.reply_to_message.message_id,
-                        message.reply_to_message.message_id + count,
-                    )
-                ],
-                replies=0,
-            )
+                        message.reply_to_message.message_id + (count + 5),
+                    ),
+                    replies=0,
+                )
+                if not i.empty and not i.media
+            ]
+            messages = messages[:count]
         else:
             if getArg(message) != "r":
                 return await m.edit(
@@ -73,9 +81,13 @@ async def quotly_func(client, message: Message):
             )
             messages = [reply_message]
     else:
-        await m.edit("Incorrect argument, check quotly module in help section.")
-        return
+        return await m.edit(
+            "Incorrect argument, check quotly module in help section."
+        )
     try:
+        if not message:
+            return await m.edit("Something went wrong.")
+
         sticker = await quotify(messages)
         if not sticker[0]:
             await message.reply_text(sticker[1])
@@ -86,12 +98,11 @@ async def quotly_func(client, message: Message):
         sticker.close()
     except Exception as e:
         await m.edit(
-            "Something wrong happened while quoting messages,"
+            "Something went wrong while quoting messages,"
             + " This error usually happens when there's a "
-            + " message containing something other than text."
+            + " message containing something other than text,"
+            + " or one of the messages in-between are deleted."
         )
         e = format_exc()
         print(e)
 
-
-__mod_name__ = "Quotly"
