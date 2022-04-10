c833fecb7cf8292e76ae6193b51d5f7703645f04
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-10 07:36:47 +0530

    Update __init__.py

--- horisan/modules/helper_funcs/telethn/__init__.py
+++ horisan/modules/helper_funcs/telethn/__init__.py
@@ -1,7 +1,7 @@
-from horisan import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES, telethn
+from horisan import DEV_USERS, DRAGONS, DEMONS, telethn
 
-IMMUNE_USERS = DRAGONS + WOLVES + DEMONS + TIGERS + DEV_USERS
+IMMUNE_USERS = DRAGONS + DEMONS + DEV_USERS
 
 IMMUNE_USERS = (
-    list(DRAGONS) + list(WOLVES) + list(DEMONS) + list(TIGERS) + list(DEV_USERS)
+    list(DRAGONS) + list(DEMONS) + list(DEV_USERS)
 )
