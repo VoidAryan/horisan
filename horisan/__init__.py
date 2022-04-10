83588764b02ba83d0e5d44c03cf0b2faf75b1c0f
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-10 08:07:25 +0530

    Update __init__.py

--- horisan/__init__.py
+++ horisan/__init__.py
@@ -229,7 +229,6 @@ else:
         sw = None
         LOGGER.warning("Can't connect to SpamWatch!")
 
-from EmikoRobot.modules.sql import SESSION
 
 defaults = tg.Defaults(run_async=True)
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
