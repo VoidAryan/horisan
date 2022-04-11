b78a262080d6f1900cb140d8c4becd60c486e5c7
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-11 18:49:21 +0530

    Update afk.py

--- horisan/modules/afk.py
+++ horisan/modules/afk.py
@@ -45,7 +45,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} See Yaa Soon!{}".format(fname, notice))
+        update.effective_message.reply_text("ᴄʏᴀ 👋: {} {}".format(fname, notice))
     except BadRequest:
          pass
 
