8097e1a89dec432dcee51c8cf30823fb9d5c4c35
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-19 21:11:31 +0530

    Update pyrogithub.py

--- horisan/modules/pyrogithub.py
+++ horisan/modules/pyrogithub.py
@@ -3,8 +3,12 @@ from pyrogram import filters
 from horisan import pbot, BOT_USERNAME
 from horisan.utils.errors import capture_err
 
+__help__ = """
+× you can search github accounts through this module
+× usage : /git (github username)
+"""
 
-__mod_name__ = "Github"
+__mod_name__ = "【ɢɪᴛʜᴜʙ】"
 
 
 @pbot.on_message(filters.command(["github", "git", f"git@{BOT_USERNAME}"]))
