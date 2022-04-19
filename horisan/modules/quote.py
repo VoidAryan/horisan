8468e4b2d8a5e3ec9dd9788458834814d8e521ab
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-19 16:44:57 +0530

    Update quote.py

--- horisan/modules/quote.py
+++ horisan/modules/quote.py
@@ -11,7 +11,7 @@ import json
 import os
 import re
 from telethon import events
-from MikuXProBot import telethn
+from horisan import telethn
 
 COLORS = [
     "#F07975", "#F49F69", "#F9C84A", "#8CC56E", "#6CC7DC", "#80C1FA", "#BCB3F9", "#E181AC"]
@@ -389,7 +389,7 @@ async def q(event):
     os.remove('sticker.webp')
     
     
-__mod_name__ = "QUOTLY🖼️"
+__mod_name__ = "【Qᴜᴏᴛʟʏ】"
 
 __help__ = """
 ⚙️ 𝗔𝘃𝗮𝗶𝗹𝗮𝗯𝗹𝗲 𝗰𝗼𝗺𝗺𝗮𝗻𝗱:
