a619e57622fb8ad1577b6ebd74b4c6dff67776c3
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-19 20:52:10 +0530

    Update reverse.py

--- horisan/modules/reverse.py
+++ horisan/modules/reverse.py
@@ -182,7 +182,7 @@ Do you know that you can search an image with a link too? /reverse picturelink <
 alternative commands :-
 • `/r`, `/grs`, `/pp`
 """
-__mod_name__ = "reverse"
+__mod_name__ = "【ʀᴇᴠᴇʀꜱᴇ】"
 
 REVERSE_HANDLER = DisableAbleCommandHandler(
     ["pp", "grs" , "r","reverse"], reverse, pass_args=True, admin_ok=True, run_async=True
