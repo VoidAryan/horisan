f4db2f7ad1a9680a5706ed86cda65606fff9a176
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-19 15:10:28 +0530

    Update anti_channel.py

--- horisan/modules/anti_channel.py
+++ horisan/modules/anti_channel.py
@@ -1,5 +1,5 @@
 from telegram.ext.filters import Filters
-from MikuXProBot.modules.helper_funcs.decorators import mikucmd, mikumsg
+from horisan.modules.helper_funcs.decorators import horicmd, horimsg
 from telegram import Update, message
 from telegram.ext import CallbackContext
 from ..modules.helper_funcs.anonymous import user_admin, AdminPerms
@@ -7,7 +7,7 @@ import html
 from ..modules.sql.antichannel_sql import antichannel_status, disable_antichannel, enable_antichannel
 
 
-@mikucmd(command="antichannel", group=100)
+@horicmd(command="antichannel", group=100)
 @user_admin(AdminPerms.CAN_RESTRICT_MEMBERS)
 def set_antichannel(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -28,7 +28,7 @@ def set_antichannel(update: Update, context: CallbackContext):
         "Antichannel setting is currently {} in {}".format(antichannel_status(chat.id), html.escape(chat.title)))
 
 
-@mikumsg(Filters.chat_type.groups, group=110)
+@horimsg(Filters.chat_type.groups, group=110)
 def eliminate_channel(update: Update, context: CallbackContext):
     message = update.effective_message
     chat = update.effective_chat
