007e20be9d637554539140875b8d6ebc9a3c2587
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-20 23:24:34 +0530

    Update welcome_sql.py

--- horisan/modules/sql/welcome_sql.py
+++ horisan/modules/sql/welcome_sql.py
@@ -2,8 +2,8 @@ import random
 import threading
 from typing import Union
 
-from EmikoRobot.modules.helper_funcs.msg_types import Types
-from EmikoRobot.modules.sql import BASE, SESSION
+from horisan.modules.helper_funcs.msg_types import Types
+from horisan.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, String, UnicodeText, Integer
 from sqlalchemy.sql.sqltypes import BigInteger
 
