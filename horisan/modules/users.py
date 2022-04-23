70701819df784e09e23d5e8be6042e54517abbbb
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-23 21:53:44 +0530

    Update users.py

--- horisan/modules/users.py
+++ horisan/modules/users.py
@@ -49,7 +49,7 @@ def get_user_id(username):
 
     return None
 
-
+@run_sync
 @dev_plus
 def broadcast(update: Update, context: CallbackContext):
     to_send = update.effective_message.text.split(None, 1)
