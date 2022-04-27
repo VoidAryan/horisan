d822288df65e3cd63b028bdff2164b0e3abc972d
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-04-27 18:44:11 +0530

    Update gshield.py

--- horisan/modules/gshield.py
+++ horisan/modules/gshield.py
@@ -114,62 +114,6 @@ approved_users = db.approve
 spammers = db.spammer
 globalchat = db.globchat
 
-CMD_STARTERS = ["/", "!", "."]
-profanity.load_censor_words_from_file("./profanity_wordlist.txt")
-
-
-@register(pattern="^/profanity(?: |$)(.*)")
-async def profanity(event):
-    if event.fwd_from:
-        return
-    if not event.is_group:
-        await event.reply("You Can Only profanity in Groups.")
-        return
-    event.pattern_match.group(1)
-    if not await is_admin(event, BOT_ID):
-        await event.reply("`I Should Be Admin To Do This!`")
-        return
-    if await is_admin(event, event.message.sender_id):
-        input = event.pattern_match.group(1)
-        chats = spammers.find({})
-        if not input:
-            for c in chats:
-                if event.chat_id == c["id"]:
-                    await event.reply(
-                        "Please provide some input yes or no.\n\nCurrent setting is : **on**"
-                    )
-                    return
-            await event.reply(
-                "Please provide some input yes or no.\n\nCurrent setting is : **off**"
-            )
-            return
-        if input == "on":
-            if event.is_group:
-                chats = spammers.find({})
-                for c in chats:
-                    if event.chat_id == c["id"]:
-                        await event.reply(
-                            "Profanity filter is already activated for this chat."
-                        )
-                        return
-                spammers.insert_one({"id": event.chat_id})
-                await event.reply("Profanity filter turned on for this chat.")
-        if input == "off":
-            if event.is_group:
-                chats = spammers.find({})
-                for c in chats:
-                    if event.chat_id == c["id"]:
-                        spammers.delete_one({"id": event.chat_id})
-                        await event.reply("Profanity filter turned off for this chat.")
-                        return
-            await event.reply("Profanity filter isn't turned on for this chat.")
-        if not input == "on" and not input == "off":
-            await event.reply("I only understand by on or off")
-            return
-    else:
-        await event.reply("`You Should Be Admin To Do This!`")
-        return
-
 
 @register(pattern="^/globalmode(?: |$)(.*)")
 async def profanity(event):
