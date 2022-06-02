6ff3214b060f78e0d70c41e8a543985bc3b9adea
HodackaX <89735428+HodackaX@users.noreply.github.com>
2022-06-02 12:58:23 +0530

    Update misc.py

--- horisan/modules/helper_funcs/misc.py
+++ horisan/modules/helper_funcs/misc.py
@@ -69,11 +69,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("🔹ᴘʀᴇᴠɪᴏᴜꜱ🔹", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("🔸BACK🔸",  callback_data="delete"),
+                EqInlineKeyboardButton("🔸BACK🔸",  callback_data="hori_back"),
              EqInlineKeyboardButton("🔹ɴᴇxᴛ🔹", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("🔸BACK🔸", callback_data="delete")]]
+        pairs += [[EqInlineKeyboardButton("🔸BACK🔸", callback_data="hori_back")]]
 
     return pairs
  
