37d5248cc97d611021b5658246c340717b7a822d
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-06-12 21:27:12 +0530

    Update Baii.py

--- horisan/modules/Baii.py
+++ horisan/modules/Baii.py
@@ -15,4 +15,5 @@ PHOTO = "https://telegra.ph/file/242d186b33c49c0329a0f.mp4"
 @register(pattern=("/afk"))
 async def awake(event):
     TEXT = f"**Baii Baii [{event.sender.first_name}](tg://user?id={event.sender.id}) ! 👋**"
-    await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
+   
+await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
