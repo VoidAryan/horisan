6813458bc0127029e54834001806d635d21a2159
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-06-12 20:01:17 +0530

    Test for fest oh yeh wush
    Hush wush repo chor

---
+++ horisan/modules/Baii.py
@@ -0,0 +1,32 @@
+from horisan import dispatcher
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ParseMode
+
+from telegram.ext import (
+    CallbackContext,
+    CommandHandler,
+)
+
+PHOTO = "https://telegra.ph/file/242d186b33c49c0329a0f.mp4"
+
+
+
+def baiibaii(update: Update, context: CallbackContext):
+
+    TEXT = f"Baii-Baii  {mention} 👋"
+
+    update.effective_message.reply_photo(
+        PHOTO, caption= TEXT,
+        parse_mode=ParseMode.MARKDOWN,
+
+            reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(text="【V๏ɪ፝֟𝔡】Network", url="https://t.me/voidxnetwork")]
+            ]
+        ),
+    )
+
+
+baiibaii_handler = CommandHandler("bye", baiibaii, run_async = True)
+dispatcher.add_handler(baiibaii_handler)
+
+__mod_name__ = "【baiibaii】"
