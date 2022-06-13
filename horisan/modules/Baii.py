24c6a163507ec842232adcaa5963fb690b69be4a
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-06-13 08:32:06 +0530

    Update Baii.py

--- horisan/modules/Baii.py
+++ horisan/modules/Baii.py
@@ -1,7 +1,6 @@
 import os
 import re
 from platform import python_version as kontol
-from telethon import events, Button
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
@@ -13,5 +12,6 @@ PHOTO = "https://telegra.ph/file/242d186b33c49c0329a0f.mp4"
 
 
 @register(pattern=("/afk"))
-
-   
+async def awake(event):
+    TEXT = f"**Baii Baii [{event.sender.first_name}](tg://user?id={event.sender.id}) 👋"
+    await tbot.send_file(event.chat_id, PHOTO, caption=TEXT)
