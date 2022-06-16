864c6c2340533b476d887659e18c9adc89b670a8
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-06-16 07:59:16 +0530

    Update __main__.py

--- horisan/__main__.py
+++ horisan/__main__.py
@@ -724,10 +724,7 @@ def get_settings(update: Update, context: CallbackContext):
     # ONLY send settings in PM
     if chat.type != chat.PRIVATE:
         if is_user_admin(chat, user.id):
-            text = "Click here to get this chat's settings, as well as yours."
-            msg.reply_text(
-                text,
-                update.effective_message.reply_photo(
+            update.effective_message.reply_photo(
                 "https://telegra.ph/file/250fa84d9d9abfc05904b.jpg",
                 caption=f"Baka 💫 contact me in PM to get help of {module.capitalize()}",
                 reply_markup=InlineKeyboardMarkup(
