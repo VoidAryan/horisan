2900ab825648bce64bd7cf8c6082e68a517aca6f
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-07-04 12:54:37 +0530

    Update blacklist.py

--- horisan/modules/blacklist.py
+++ horisan/modules/blacklist.py
@@ -444,7 +444,7 @@ def __chat_settings__(chat_id, user_id):
 
 
 def __stats__():
-    return "× {} blacklist triggers, across {} chats.".format(
+    return "۞  ʙʟᴀᴄᴋʟɪsᴛ  ᴛʀɪɢɢᴇʀs : {} | ᴄʜᴀᴛs : {}\n".format(
         sql.num_blacklist_filters(),
         sql.num_blacklist_filter_chats(),
     )
