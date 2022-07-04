b37d2ab25286c59e66d13ae2603ac69d8d768df0
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-07-04 12:42:19 +0530

    Update notes.py

--- horisan/modules/notes.py
+++ horisan/modules/notes.py
@@ -534,7 +534,7 @@ def __import_data__(chat_id, data):
 
 
 def __stats__():
-    return f"× {sql.num_notes()} notes, across {sql.num_chats()} chats."
+    return f"۞ ɴᴏᴛᴇs : {sql.num_notes()} | ᴄʜᴀᴛs : {sql.num_chats()}\n"
 
 
 def __migrate__(old_chat_id, new_chat_id):
