b8ec9f50370d86fc709b5941bdc4699bf8878f8a
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-07-10 17:56:18 +0530

    Update misc.py

--- horisan/modules/helper_funcs/misc.py
+++ horisan/modules/helper_funcs/misc.py
@@ -2,7 +2,7 @@ from math import ceil
 from typing import Dict, List
 
 from horisan import NO_LOAD
-from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
+from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, InputTextMessageContent, InlineQueryResultArticle
 from telegram.error import TelegramError
 
 
