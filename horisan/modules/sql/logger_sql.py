1f017d44e83612b5ff5755c8350da0633ebb371a
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-07-24 14:09:02 +0530

    Update logger_sql.py

--- horisan/modules/sql/logger_sql.py
+++ horisan/modules/sql/logger_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from MikuXProBot.modules.sql import BASE, SESSION
+from horisan.modules.sql import BASE, SESSION
 
 class LoggerSettings(BASE):
     __tablename__ = "chat_log_settings"
