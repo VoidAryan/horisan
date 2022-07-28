0117229af09611a745b038fcb640b1028b1dd8c5
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-07-28 15:21:47 +0530

    Update welcome.py

--- horisan/modules/welcome.py
+++ horisan/modules/welcome.py
@@ -193,7 +193,7 @@ def new_member(update: Update, context: CallbackContext):
 
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
-                update.effective_message.reply_video(
+                update.effective_message.reply_photo(
                 MIKU_IMG, caption= "UwU My Owner Just Joined !!",
                     reply_to_message_id=reply)
                 welcome_log = (f"{html.escape(chat.title)}\n"
