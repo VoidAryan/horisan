3998e1d2f582bd0cf37a8a1a4ade93950710b111
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-07-28 08:38:37 +0530

    Update welcome.py

--- horisan/modules/welcome.py
+++ horisan/modules/welcome.py
@@ -1022,59 +1022,6 @@ Main commands available :
    • in a group: will redirect you to pm, with all that chat's settings.
 For all command use / or !
 """
- 
-def help_button(update, context):
-    query = update.callback_query
-    bot = context.bot
-    mod_match = re.match(r"help_module\((.+?)\)", query.data)
-    prev_match = re.match(r"help_prev\((.+?)\)", query.data)
-    next_match = re.match(r"help_next\((.+?)\)", query.data)
-    back_match = re.match(r"help_back", query.data)
-
-    print(query.message.chat.id)
-
-    try:
-        if mod_match:
-            module = mod_match.group(1)
-            text = ("Here is the help for the *{}* module:\n".format(
-                HELPABLE[module].__mod_name__) + HELPABLE[module].__help__)
-            query.message.edit_caption(
-                text,
-                parse_mode=ParseMode.MARKDOWN,
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Back",
-                                       callback_data="help_back"),
-                  InlineKeyboardButton(text="Support",
-                                       url="t.me/Mikuxsupport")]]))
-
-        elif prev_match:
-            curr_page = int(prev_match.group(1))
-            query.message.edit_caption(
-                HELP_STRINGS.format(update.effective_user.first_name, update.effective_user.id),
-                parse_mode=ParseMode.MARKDOWN,
-                reply_markup=InlineKeyboardMarkup(
-                    paginate_modules(curr_page - 1, HELPABLE, "help")))
-
-        elif next_match:
-            next_page = int(next_match.group(1))
-            query.message.edit_caption(
-                HELP_STRINGS.format(update.effective_user.first_name, update.effective_user.id),
-                parse_mode=ParseMode.MARKDOWN,
-                reply_markup=InlineKeyboardMarkup(
-                    paginate_modules(next_page + 1, HELPABLE, "help")))
-
-        elif back_match:
-            query.message.edit_caption(
-                HELP_STRINGS.format(update.effective_user.first_name, update.effective_user.id),
-                parse_mode=ParseMode.MARKDOWN,
-                reply_markup=InlineKeyboardMarkup(
-                    paginate_modules(0, HELPABLE, "help")))
-
-        # ensure no spinny white circle
-        context.bot.answer_callback_query(query.id)
-        # query.message.delete()
-
-    except BadRequest:
-        pass
 
 __help__ = """
 *Admins only:*
