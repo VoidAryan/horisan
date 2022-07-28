30643cd59221e54e851ffd7ae2fcfaefad95138b
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-07-28 21:19:08 +0530

    Update misc.py

--- horisan/modules/helper_funcs/misc.py
+++ horisan/modules/helper_funcs/misc.py
@@ -79,15 +79,15 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     calc = len(modules) - round(round_num)
     if calc == 1:
         pairs.append((modules[-1], ))
-    elif calc == 2:
+    elif calc == 3:
         pairs.append((modules[-1], ))
 
     max_num_pages = ceil(len(pairs) / 10)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 5:
-        pairs = pairs[modulo_page * 5:5 * (modulo_page + 2)] + [
+    if len(pairs) > 3:
+        pairs = pairs[modulo_page * 5:5 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("🔹ᴘʀᴇᴠɪᴏᴜꜱ🔹", callback_data="{}_prev({})".format(prefix, modulo_page)),
                 EqInlineKeyboardButton("🔸BACK🔸",  callback_data="hori_back"),
              EqInlineKeyboardButton("🔹ɴᴇxᴛ🔹", callback_data="{}_next({})".format(prefix, modulo_page)))]
