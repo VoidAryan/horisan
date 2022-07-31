aa0bf2394086ad15ecc261336f11a56af7681f60
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-07-30 23:59:18 -0700

    Update animeflv.py

--- horisan/modules/animeflv.py
+++ horisan/modules/animeflv.py
@@ -2,12 +2,13 @@ from animeflv import AnimeFLV
 api = AnimeFLV()
 from pyrogram import filters
 from horisan import pbot as bot
+import requests
 
 @bot.on_message(filters.command("sr"))
 async def anime(_, message):
     query = " ".join(message.command[1:])
     uff = api.search('query')
-    name = uff.list["title"]
-    rat = uff.list["rating"]
-    pic = uff.list["banner"]
+    name = uff.get["title"]
+    rat = uff.get["rating"]
+    pic = uff.get["banner"]
     await message.reply_photo(pic, caption=name)
