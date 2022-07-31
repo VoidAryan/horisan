3eb0335db67347a557d00a7c4362db2bbfeb37b1
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-07-30 23:02:45 -0700

    Create animeflv.py

---
+++ horisan/modules/animeflv.py
@@ -0,0 +1,13 @@
+from animeflv import AnimeFLV
+api = AnimeFLV()
+from pyrogram import filters
+from horisan import pbot as bot
+
+@bot.on_message(filters.command("sr"))
+async def anime(_, message):
+    query = " ".join(message.command[1:])
+    uff = api.search('query')
+    name = uff['title']
+    rat = uff['rating']
+    pic = uff['banner']
+    await message.reply_photo(pic, caption=name)
