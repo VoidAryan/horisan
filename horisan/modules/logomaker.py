eb66eda905be510677f9bb8372478c8f0bce38ac
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-08-01 18:36:13 +0530

    Create logomaker.py

---
+++ horisan/modules/logomaker.py
@@ -0,0 +1,121 @@
+import os
+import requests
+import io
+import random
+import glob
+from PIL import Image, ImageDraw, ImageFont
+from telethon.tl.types import InputMessagesFilterPhotos
+from MikuXProBot.events import register
+from MikuXProBot import telethn as tbot
+from telethon import Button, custom
+def mediainfo(media):
+    xx = str((str(media)).split("(", maxsplit=1)[0])
+    m = ""
+    if xx == "MessageMediaDocument":
+        mim = media.document.mime_type
+        if mim == "application/x-tgsticker":
+            m = "sticker animated"
+        elif "image" in mim:
+            if mim == "image/webp":
+                m = "sticker"
+            elif mim == "image/gif":
+                m = "gif as doc"
+            else:
+                m = "pic as doc"
+        elif "video" in mim:
+            if "DocumentAttributeAnimated" in str(media):
+                m = "gif"
+            elif "DocumentAttributeVideo" in str(media):
+                i = str(media.document.attributes[0])
+                if "supports_streaming=True" in i:
+                    m = "video"
+                m = "video as doc"
+            else:
+                m = "video"
+        elif "audio" in mim:
+            m = "audio"
+        else:
+            m = "document"
+    elif xx == "MessageMediaPhoto":
+        m = "pic"
+    elif xx == "MessageMediaWebPage":
+        m = "web"
+    return m
+
+
+@register(pattern="^/logo ?(.*)")
+async def logo_gen(event):
+    xx = await event.reply("`Processing...`")
+    name = event.pattern_match.group(1)
+    if not name:
+        await xx.edit("`Please gimme Some Text To Draw`")
+        return
+    bg_, font_ = "", ""
+    if event.reply_to_msg_id:
+        temp = await event.get_reply_message()
+        if temp.media:
+            if hasattr(temp.media, "document"):
+                if "font" in temp.file.mime_type:
+                    font_ = await temp.download_media()
+                elif (".ttf" in temp.file.name) or (".otf" in temp.file.name):
+                    font_ = await temp.download_media()
+            elif "pic" in mediainfo(temp.media):
+                bg_ = await temp.download_media()
+    else:
+        pics = [         "https://telegra.ph/file/adf6be85ec162044e9fbe.jpg"
+               ]
+        id_ = random.choice(pics)
+        fpath_ = glob.glob("./MikuXProBot/resources/Vampire Wars.otf")
+        font_ = random.choice(fpath_)
+    if not bg_:
+        pics = []
+        id_ = random.choice(pics)
+        img = Image.open(io.BytesIO(requests.get(id_).content))
+    if not font_:
+        fpath_ = glob.glob("./MikuXProBot/resources/Vampire Wars.otf")
+        font_ = random.choice(fpath_)
+    if len(name) <= 8:
+        fnt_size = 120
+        strke = 10
+    elif len(name) >= 9:
+        fnt_size = 50
+        strke = 5
+    else:
+        fnt_size = 100
+        strke = 20
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font_, fnt_size)
+    w, h = draw.textsize(name, font=font)
+    h += int(h * 0.21)
+    image_width, image_height = img.size
+    draw.text(
+        ((image_width - w) / 2, (image_height - h) / 2),
+        name,
+        font=font,
+        fill=(255, 255, 255),
+    )
+    x = (image_width - w) / 2
+    y = (image_height - h) / 2
+    draw.text((x, y), name, font=font, fill="white",
+              stroke_width=strke, stroke_fill="black")
+    flnme = f"logo.png"
+    BUTTON = [[Button.url("Support Chat", "https://t.me/MikusSupport"), Button.url("Updates Channel", "https://t.me/MikuXUpdates")]]
+    img.save(flnme, "png")
+    await xx.edit("`Uploading`")
+    if os.path.exists(flnme):
+        await tbot.send_file(
+            event.chat_id,
+            file=flnme,
+            caption=f"Logo by [{event.sender.first_name}](tg://user?id={event.sender.id}) Via [Miku](https://t.me/Mikuxprobot)",
+            buttons=BUTTON,
+        )
+        os.remove(flnme)
+        await xx.delete()
+    if os.path.exists(bg_):
+        os.remove(bg_) 
+    if os.path.exists(font_):
+        if not font_.startswith("./MikuXProBot/resources/Vampire Wars.otf"):
+            os.remove(font_)
+
+
+__mod_name__ = "Logomaker"
