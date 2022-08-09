ec7955fee7d8536e51fe3e8ed6718da1c965a672
Hyper <70685160+hyper-ub@users.noreply.github.com>
2022-08-09 14:08:06 +0530

    Update blsticker_sql.py

--- horisan/modules/sql/blsticker_sql.py
+++ horisan/modules/sql/blsticker_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, distinct, func, Integer
 
-from horisan.modules.sql import BASE, SESSION
+from horisan.modules.sql.db2 import BASE, SESSION
 
 
 class StickersFilters(BASE):
