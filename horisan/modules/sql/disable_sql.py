f6f10aa55baec56d5b5a78cddf83b8d4bee574db
Hyper <70685160+hyper-ub@users.noreply.github.com>
2022-08-09 14:10:10 +0530

    Update disable_sql.py

--- horisan/modules/sql/disable_sql.py
+++ horisan/modules/sql/disable_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, distinct, func
 
-from horisan.modules.sql import BASE, SESSION
+from horisan.modules.sql.db2 import BASE, SESSION
 
 
 class Disable(BASE):
