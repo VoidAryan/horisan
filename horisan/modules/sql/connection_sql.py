1556ff92e4accfe8f1d6f02b3953a0c4638a161f
Hyper <70685160+hyper-ub@users.noreply.github.com>
2022-08-09 14:05:54 +0530

    Update connection_sql.py

--- horisan/modules/sql/connection_sql.py
+++ horisan/modules/sql/connection_sql.py
@@ -5,7 +5,7 @@ from typing import Union
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 from sqlalchemy.sql.sqltypes import BigInteger
 
-from horisan.modules.sql import SESSION, BASE
+from horisan.modules.sql.db2 import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
