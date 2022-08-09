0847a187c8034a69a33240da4747530696fb2f59
Hyper <70685160+hyper-ub@users.noreply.github.com>
2022-08-09 14:07:38 +0530

    Update blacklistusers_sql.py

--- horisan/modules/sql/blacklistusers_sql.py
+++ horisan/modules/sql/blacklistusers_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText
 
-from horisan.modules.sql import BASE, SESSION
+from horisan.modules.sql.db2 import BASE, SESSION
 
 
 class BlacklistUsers(BASE):
