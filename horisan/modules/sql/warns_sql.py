460d448dba324669c13f9d48a2a2a78443aae502
Hyper <70685160+hyper-ub@users.noreply.github.com>
2022-08-09 14:09:46 +0530

    Update warns_sql.py

--- horisan/modules/sql/warns_sql.py
+++ horisan/modules/sql/warns_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from horisan.modules.sql import BASE, SESSION
+from horisan.modules.sql.db3 import BASE, SESSION
 from sqlalchemy import Boolean, Column, String, UnicodeText, distinct, func, Integer
 from sqlalchemy.sql.sqltypes import BigInteger
 from sqlalchemy.dialects import postgresql
