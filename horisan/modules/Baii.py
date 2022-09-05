aae2bd6f0bc1b8076708b7a079fa1e77455e37b3
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-09-05 17:53:19 +0530

    Update Baii.py

--- horisan/modules/Baii.py
+++ horisan/modules/Baii.py
@@ -1,9 +1,5 @@
-import os
+"""import os
 import re
-from platform import python_version as kontol
-from telegram import __version__ as telever
-from telethon import __version__ as tlhver
-from pyrogram import __version__ as pyrover
 from horisan.events import register
 from horisan import telethn as tbot
 
@@ -15,3 +11,4 @@ PHOTO = "https://telegra.ph/file/242d186b33c49c0329a0f.mp4"
 async def awake(event):
     TEXT = f"Baii Baii [{event.sender.first_name}](tg://user?id={event.sender.id}) 👋"
     await tbot.send_file(event.chat_id, PHOTO, caption=TEXT)
+"""
