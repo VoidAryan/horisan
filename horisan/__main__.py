d286fa0c6197883125660ba5d81df16f26c90782
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-09-06 09:36:34 +0530

    Update __main__.py

--- horisan/__main__.py
+++ horisan/__main__.py
@@ -769,7 +769,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.send(
+            dispatcher.bot.send_message(
                 f"@{SUPPORT_CHAT}",
                 "ɪ ᴀᴍ ᴀʟɪᴠᴇ ᴠᴏɪᴅ ꜱᴀᴍᴀ 🥀",
                 parse_mode=ParseMode.MARKDOWN,
