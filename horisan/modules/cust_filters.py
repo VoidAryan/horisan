a58699f02ba8ee5845a6fa2fd860ac8328b8774a
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-09-07 14:39:22 +0530

    Update cust_filters.py

--- horisan/modules/cust_filters.py
+++ horisan/modules/cust_filters.py
@@ -548,7 +548,7 @@ doin?
 Check `/markdownhelp` to know more!
 """
 
-__mod_name__ = "Filters"
+__mod_name__ = "ꜰɪʟᴛᴇʀ"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)  # Don't Use async for this
