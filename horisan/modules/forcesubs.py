69a829b73707c1d80f9fca3dacb1d22cbf927488
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-09-07 15:13:04 +0530

    Update forcesubs.py

--- horisan/modules/forcesubs.py
+++ horisan/modules/forcesubs.py
@@ -204,4 +204,4 @@ Feds are now divided into 3 sections for your ease.
 • `/fedadminhelp`*:* Provides help for fed administration commands
 • `/feduserhelp`*:* Provides help for commands anyone can use
 """
-__mod_name__ = "【ꜰ-ꜱᴜʙ/ꜰᴇᴅ】"
+__mod_name__ = "ꜰ-ꜱᴜʙ/ꜰᴇᴅ"
